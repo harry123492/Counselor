52aef6f066cdb6ccc4adb2c6e15289e8485f5ac4
Jeon__ih <wjs_5025@naver.com>
2020-10-13 22:09:10 +0900

    주석처리

--- app/src/main/java/com/example/counselor/MainActivity.java
+++ app/src/main/java/com/example/counselor/MainActivity.java
@@ -5,14 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 }
-aaaaalml
-kdh
-lmlmlmlmlmlmlmlmlslsllsss
-ssddssdd
+
+//aaaaalml
+//kdh
+//lmlmlmlmlmlmlmlmlslsllsss
+//ssddssdd
