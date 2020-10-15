414c41c22ef55ceaceaefc210272d9ddbf707a08
Jeon__ih <wjs_5025@naver.com>
2020-10-15 15:44:27 +0900

    경상대학교 마커찍기까지

--- app/src/main/java/com/example/counselor/MainActivity.java
+++ app/src/main/java/com/example/counselor/MainActivity.java
@@ -9,7 +9,9 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.internal.ICameraUpdateFactoryDelegate;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     private GoogleMap googleMap;
@@ -29,6 +31,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         //35.153554, 128.099335 경상대학교
         LatLng latLng = new LatLng(35.153554, 128.099335);
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        googleMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("경상대학교");
+        googleMap.addMarker(markerOptions);
     }
 }
 
