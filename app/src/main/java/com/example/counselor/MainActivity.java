188ea2a37b439e7ae3a528e9f1bd1b10b84877eb
Jeon__ih <wjs_5025@naver.com>
2020-10-15 15:31:57 +0900

    경상대학교 위도경도까지

--- app/src/main/java/com/example/counselor/MainActivity.java
+++ app/src/main/java/com/example/counselor/MainActivity.java
@@ -4,12 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-    @Override
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap googleMap;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        //35.153554, 128.099335 경상대학교
+        LatLng latLng = new LatLng(35.153554, 128.099335);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
     }
 }
 
