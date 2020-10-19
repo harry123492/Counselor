730e34bc4abf4486f1bcd3c6c466e4fa1c82d446
harry123492 <harry123492@naevr.com>
2020-10-19 20:11:52 +0900

    심리탭 정보넘어가기

--- app/src/main/java/com/example/counselor/Family.java
+++ app/src/main/java/com/example/counselor/Family.java
@@ -4,19 +4,40 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class Family extends Fragment {
 
     private View view;
+    private LinearLayout dodream;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.famaily, container, false);
+
+        dodream = view.findViewById(R.id.dodream);
+
+        dodream.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle bundle = new Bundle();
+                bundle.putString("fromMenu", "메뉴");
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+
+                Dodream dodream = new Dodream();
+                dodream.setArguments(bundle);
+                transaction.replace(R.id.main_frame, dodream);
+                transaction.commit();
+
+
+            }
+        });
 
         return view;
     }
--- app/src/main/java/com/example/counselor/Hearing.java
+++ app/src/main/java/com/example/counselor/Hearing.java
@@ -17,6 +17,11 @@ public class Hearing extends Fragment {
 
     private View view;
     private LinearLayout dodream;
+    private LinearLayout sunflower;
+    private LinearLayout miso;
+    private LinearLayout jinju;
+    private LinearLayout toktok;
+
 
     @Nullable
     @Override
@@ -37,8 +42,96 @@ public class Hearing extends Fragment {
                 transaction.replace(R.id.main_frame, dodream);
                 transaction.commit();
 
+
+
+
             }
         });
+
+        sunflower = view.findViewById(R.id.sunflower);
+
+        sunflower.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle bundle = new Bundle();
+                bundle.putString("fromMenu", "메뉴");
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+
+                Sunflower sunflower = new Sunflower();
+                sunflower.setArguments(bundle);
+                transaction.replace(R.id.main_frame, sunflower);
+                transaction.commit();
+
+
+
+
+            }
+        });
+
+
+        miso = view.findViewById(R.id.miso);
+
+        miso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle bundle = new Bundle();
+                bundle.putString("fromMenu", "메뉴");
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+
+                Miso miso = new Miso();
+                miso.setArguments(bundle);
+                transaction.replace(R.id.main_frame, miso);
+                transaction.commit();
+
+
+
+
+            }
+        });
+
+
+        jinju = view.findViewById(R.id.jinju);
+
+        jinju.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle bundle = new Bundle();
+                bundle.putString("fromMenu", "메뉴");
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+
+                Jinju jinju = new Jinju();
+                jinju.setArguments(bundle);
+                transaction.replace(R.id.main_frame, jinju);
+                transaction.commit();
+
+
+
+
+            }
+        });
+
+
+        toktok = view.findViewById(R.id.toktok);
+
+        toktok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle bundle = new Bundle();
+                bundle.putString("fromMenu", "메뉴");
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+
+                Toktok toktok = new Toktok();
+                toktok.setArguments(bundle);
+                transaction.replace(R.id.main_frame, toktok);
+                transaction.commit();
+
+
+
+
+            }
+        });
+
+
 
         return view;
     }
