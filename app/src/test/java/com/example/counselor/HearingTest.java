b5e4916adbd042a54f8baaba57405250a45d07ff
hwalang <smdong97@gmail.com>
2020-11-26 16:41:56 +0900

    gradle test추가

---
+++ app/src/test/java/com/example/counselor/HearingTest.java
@@ -0,0 +1,13 @@
+package com.example.counselor;
+
+import junit.framework.TestCase;
+
+public class HearingTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+}
